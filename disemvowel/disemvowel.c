5a18657d10daa56b67a9aaadc72c893f67ec72e2
Jeffrey Miller <mill7707@bebop.morris.umn.edu>
2021-09-28 10:46:27 -0500

    Adds function that checks whether a certain character is a vowel.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -4,6 +4,32 @@
 
 #include "disemvowel.h"
 
+bool isVowel(char let){
+	
+  switch(let) {
+  	case 'a':
+	case 'A':
+	  return true:
+	case 'e': 
+        case 'E':
+          return true;
+	case 'i': 
+        case 'I':
+          return true;
+	case 'o': 
+        case 'O':
+          return true;
+	case 'u': 
+        case 'U':
+          return true;
+	default: 
+	  return false;
+  }
+}
+
+
 char *disemvowel(char *str) {
-  return (char*) "";
+  
+  char *str_no_const
+  return (char*) str;
 }
